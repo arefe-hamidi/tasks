5af5f5cb041906bb83cddbbbbe313c2ffe292802
arefe_hamidi <arefehamidi.guilan@gmail.com>
2023-08-23 16:00:22 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+
+function App() {
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
